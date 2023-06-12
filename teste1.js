4a1ae4032cc7714ace360e858d3a8f6c64d7fc2c
jcaluiz <lacjunior129.la@gmail.com>
2023-06-12 11:06:37 -0300

    Função teste1 compara o nome na query, considerando hífen como espaços, desconsiderando maiúsculas e minúsculas e tirando acentos, com os nomes do banco de dados

--- teste1.js
+++ teste1.js
@@ -1,21 +1,24 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res, _next) => {
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
+    const { name: nameQuery } = req.query;
+    // Deixar tudo minúsculo e tirar o hífen do 'name' e colocar espaços no lugar
+    const hyphenForSpaceQuery = nameQuery.replace(/-/gm, " ").toLowerCase();
 
+    const findUser = data.find(({ name }) => {
+        // Tirar os acentos dos nomes do banco de dados e deixar tudo minúsculo
+        const nameDBWithoutAccents = name.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            .toLowerCase();
+        return nameDBWithoutAccents === hyphenForSpaceQuery;
+    });
+
+    if (findUser) return res.status(200).json(findUser);
+    res.status(404).json({message: "Name not found"});
 };
 
-const getUsers = ( req, res, next ) => {
-    
+const getUsers = (req, res, next) => {
     res.send(data);
-    
 };
 
 module.exports = {
